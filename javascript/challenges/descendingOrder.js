c0fc2b441316b1029636456281f52322a2f7e6fe
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-12-13 18:30:51 -0300

    new: descendingOrder

---
+++ javascript/challenges/descendingOrder.js
@@ -0,0 +1,20 @@
+/* DESCRIPTION:
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order.
+ Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+*/
+const sortByDescending = (arr = []) => {
+	return arr.sort((a, b) => b - a);
+};
+const descendingOrder = (n = 0) => {
+	let arr = n.toString().split("");
+	arr = sortByDescending(arr);
+
+	return arr.join("");
+};
